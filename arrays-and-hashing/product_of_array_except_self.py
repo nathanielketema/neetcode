acca2d401b672c4901744044f4c2145fa5d50cd5
Nathaniel Ketema <nathanielketema@gmail.com>
2025-11-23 10:53:15 -0600

    feat: update with a better understanding

--- arrays-and-hashing/product_of_array_except_self.py
+++ arrays-and-hashing/product_of_array_except_self.py
@@ -15,18 +15,51 @@ Input: nums = [-1,0,1,2,3]
 Output: [0,-6,0,0,0]
 """
 
+
 class Solution:
+    def productExceptSelf(self, nums: list[int]) -> list[int]:
+        product, zeros = 1, 0
+        for num in nums:
+            if num == 0:
+                zeros += 1
+            else:
+                product *= num
+
+        result: list[int] = [0] * len(nums)
+        if zeros > 1:
+            return result
+
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                result[i] = product
+            elif zeros == 1:
+                result[i] = 0
+            else:
+                result[i] = int(product / nums[i])
+        return result
+
+solution = Solution()
+nums = [1,2,4,6]
+assert solution.productExceptSelf(nums) == [48,24,12,8]
+
+
+
+class Solution2:
     def productExceptSelf(self, nums: list[int]) -> list[int]:
         result: list[int] = [1] * len(nums)
 
-        prefix = 1
+        left_product = 1
         for i in range(len(nums)):
-            result[i] = prefix
-            prefix *= nums[i]
+            result[i] = left_product
+            left_product *= nums[i]
 
-        postfix = 1
+        right_product = 1
         for i in range(len(nums) - 1, -1, -1):
-            result[i] *= postfix
-            postfix *= nums[i]
+            result[i] *= right_product
+            right_product *= nums[i]
 
         return result
+
+solution = Solution()
+nums = [1,2,4,6]
+assert solution.productExceptSelf(nums) == [48,24,12,8]
