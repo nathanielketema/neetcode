6e95e22fbfff1fc7f3c37bd92b32cfdef2de1dbc
Nathaniel Ketema <nathanielketema@gmail.com>
2026-01-27 11:24:40 -0600

    feat: car fleet

--- stack/car_fleet.py
+++ stack/car_fleet.py
@@ -8,13 +8,13 @@ You are given two arrays of integers position and speed, both of length n.
 
 The destination is at position target miles.
 
-A car can not pass another car ahead of it. It can only catch up to another car 
+A car can not pass another car ahead of it. It can only catch up to another car
 and then drive at the same speed as the car ahead of it.
 
 A car fleet is a non-empty set of cars driving at the same position and same speed.
 A single car is also considered a car fleet.
 
-If a car catches up to a car fleet the moment the fleet reaches the destination, 
+If a car catches up to a car fleet the moment the fleet reaches the destination,
 then the car is considered to be part of the fleet.
 
 Return the number of different car fleets that will arrive at the destination.
@@ -23,15 +23,15 @@ Example 1:
 Input: target = 10, position = [1,4], speed = [3,2]
 Output: 1
 
-Explanation: The cars starting at 1 (speed 3) and 4 (speed 2) become a fleet, 
+Explanation: The cars starting at 1 (speed 3) and 4 (speed 2) become a fleet,
 meeting each other at 10, the destination.
 
 Example 2:
 Input: target = 10, position = [4,1,0,7], speed = [2,2,1,1]
 Output: 3
 
-Explanation: The cars starting at 4 and 7 become a fleet at position 10. The 
-cars starting at 1 and 0 never catch up to the car ahead of them. Thus, there 
+Explanation: The cars starting at 4 and 7 become a fleet at position 10. The
+cars starting at 1 and 0 never catch up to the car ahead of them. Thus, there
 are 3 car fleets that will arrive at the destination.
 
 Constraints:
@@ -43,14 +43,22 @@ Constraints:
     All the values of position are unique.
 """
 
+
 class Solution:
     def carFleet(self, target: int, position: list[int], speed: list[int]) -> int:
-        pair = [(p, s) for p, s in zip(position, speed)]
-        pair.sort(reverse=True)
-        stack: list[float] = []
+        cars = sorted(zip(position, speed), reverse=True)
 
-        for p, s in pair:
-            stack.append((target - p) / s)
+        stack = []
+        for position, speed in cars:
+            time = (target - position) / speed
+            stack.append(time)
+
             if len(stack) >= 2 and stack[-1] <= stack[-2]:
-                _ = stack.pop()
+                stack.pop()
         return len(stack)
+
+
+solution = Solution()
+target, position, speed = 10, [4, 1, 0, 7], [2, 2, 1, 1]
+assert solution.carFleet(target, position, speed) == 3
+print("It worked")
